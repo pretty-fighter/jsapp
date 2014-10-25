97c27e7bd4365a0d9a5ad36ebb5c9aba4564a82d
pretty-fighter <niche.search102@gmail.com>
2014-10-25 22:30:57 +0000

    a

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['jsapp'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '055e389f781e0b0a6d4a1ddb5b0ae6c632c210a2',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
---
+++ server.js
@@ -0,0 +1,10 @@
+require('newrelic');
+var express = require('express'),
+    app = express();
+    
+app
+    .use(express.static('./public'))
+    .get('*', function(req, res) {
+        res.sendfile('./public/main.html');
+    })
+    .listen(3000);
